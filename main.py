7e7a5ea8c07285f1318227bbdf9d701347fd3c29
Vaishnavi Gupta <gvaishnavi208@gmail.com>
2026-03-22 14:00:07 +0530

    Step 3: Added update and delete functionality (Full CRUD)"

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main():
         print("\n==== TASK MANAGER ====")
         print("1. Add Task")
         print("2. View Tasks")
-        print("3. Exit")
+        print("3. Update Task")
+        print("4. Delete Task")
+        print("5. Exit")
 
         choice = input("Enter your choice: ")
 
@@ -19,6 +21,14 @@ def main():
             manager.view_tasks()
 
         elif choice == "3":
+            task_id = int(input("Enter task ID: "))
+            manager.update_task(task_id)
+
+        elif choice == "4":
+            task_id = int(input("Enter task ID: "))
+            manager.delete_task(task_id)
+
+        elif choice == "5":
             print("👋 Exiting...")
             break
 
--- task_manager.py
+++ task_manager.py
@@ -21,3 +21,35 @@ class TaskManager:
         print("\n📋 Your Tasks:")
         for task in self.tasks:
             print(task)
+    # Update Task
+    def update_task(self, task_id):
+        for task in self.tasks:
+            if task.id == task_id:
+                print("1. Mark as Completed")
+                print("2. Change Title")
+                choice = input("Enter choice: ")
+
+                if choice == "1":
+                    task.is_completed = True
+                    print("✅ Task marked as completed!")
+
+                elif choice == "2":
+                    new_title = input("Enter new title: ")
+                    task.title = new_title
+                    print("✏️ Task updated!")
+
+                else:
+                    print("❌ Invalid choice!")
+                return
+
+        print("⚠️ Task not found!")
+
+    # Delete Task
+    def delete_task(self, task_id):
+        for task in self.tasks:
+            if task.id == task_id:
+                self.tasks.remove(task)
+                print("🗑️ Task deleted!")
+                return
+
+        print("⚠️ Task not found!")
